1e05accfc8c9c1498c6626090af4db61a1fc8eb2
Mia Pilchová <pilchova@lilianaa.dev>
2026-03-01 21:26:09 +0100

    remove unused tailwind

--- src/templates/layouts/lesson.php
+++ src/templates/layouts/lesson.php
@@ -37,7 +37,6 @@ if (isset($pageCtx['templatePath'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?= $title ? $esc($title) . " | " : "" ?> <?= $esc($globalTitle ?? '') ?></title>
   <link rel="stylesheet" href="/style.css">
-  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
 <body class="page-container">
